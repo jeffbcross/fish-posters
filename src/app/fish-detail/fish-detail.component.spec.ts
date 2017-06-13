c090c175f09fde506495ae327c1506bc9d7bfae8
Jeff Cross <middlefloor@gmail.com>
2017-06-13 01:08:55 -0700

    STEP 5: Add view to fish detail

--- src/app/fish-detail/fish-detail.component.spec.ts
+++ src/app/fish-detail/fish-detail.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FishDetailComponent } from './fish-detail.component';
+import { By } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { ProductsService } from 'app/products.service';
 
 describe('FishDetailComponent', () => {
   let component: FishDetailComponent;
@@ -8,7 +12,26 @@ describe('FishDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FishDetailComponent ]
+      declarations: [ FishDetailComponent ],
+      providers: [{
+        provide: ActivatedRoute,
+        useValue: {
+          paramMap: new BehaviorSubject(new Map([['id', 99]]))
+        }
+      }, {
+        provide: ProductsService,
+        useValue: {
+          fish: [{
+            id: 99,
+            name: 'Puffer',
+            pic: '/assets/halibut-full.jpg'
+          }, {
+            id: 100,
+            name: 'Dory',
+            pic: '/assets/squid-full.jpg'
+          }]
+        }
+      }]
     })
     .compileComponents();
   }));
@@ -21,5 +44,13 @@ describe('FishDetailComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a title for the fish', () => {
+    expect(fixture.debugElement.query(By.css('h1.title')).nativeElement.textContent).toBe('Puffer');
+  });
+
+  it('should have an image with alt text', () => {
+    expect(fixture.debugElement.query(By.css('img')).nativeElement.getAttribute('alt')).toBe('Puffer preview');
   });
 });
--- src/app/fish-detail/fish-detail.component.ts
+++ src/app/fish-detail/fish-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService, Fish } from 'app/products.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-fish-detail',
@@ -6,10 +10,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fish-detail.component.scss']
 })
 export class FishDetailComponent implements OnInit {
-
-  constructor() { }
+  product$: Observable<Fish>;
+  constructor(private route: ActivatedRoute, private products: ProductsService) { }
 
   ngOnInit() {
+    this.product$ = this.route.paramMap
+      .map(paramMap => parseInt(paramMap.get('id'), 10))
+      .map(id => this.products.fish
+          .filter(fish => fish.id === id)[0]);
   }
-
 }
